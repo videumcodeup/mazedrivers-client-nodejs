8fe7996bcebc7ef7bce91d3bccca6728c310492f
Albert Arvidsson <albert.arvidsson@gmail.com>
2016-09-15 18:32:15 +0200

    Initial example client in nodejs

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import ws from 'nodejs-websocket'
+
+const nickname = process.env.NICKNAME || 'Demo'
+
+const token = process.argv[2] ? process.argv[2].trim() : undefined
+
+const conn = ws.connect('ws://localhost:8001', {}, () => {
+  console.log('Connected')
+
+  const sendAction = (type, payload) =>
+    conn.sendText(JSON.stringify({ type, payload }))
+
+  if (token) {
+    sendAction('RESUME_REQUEST', { token })
+  } else {
+    sendAction('JOIN_REQUEST', { nickname })
+  }
+})
+
+conn.on('text', data => {
+  console.log(data)
+})
