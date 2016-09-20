a75d7561545fe227f9ded86e35c707338e7090a0
Albert Arvidsson <albert.arvidsson@gmail.com>
2016-09-20 22:41:39 +0200

    Use separate connect event handler
    Allows usage of sendAction helper both inside and outside it.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const nickname = process.env.NICKNAME || 'Demo'
 
 const token = process.argv[2] ? process.argv[2].trim() : undefined
 
-const conn = ws.connect('ws://localhost:8001', {}, () => {
-  console.log('Connected')
+const conn = ws.connect('ws://localhost:8001', {})
 
-  const sendAction = (type, payload) =>
-    conn.sendText(JSON.stringify({ type, payload }))
+const sendAction = (type, payload) =>
+  conn.sendText(JSON.stringify({ type, payload }))
+
+conn.on('connect', () => {
+  console.log('Connected')
 
   if (token) {
     sendAction('REJOIN_REQUEST', { token })
